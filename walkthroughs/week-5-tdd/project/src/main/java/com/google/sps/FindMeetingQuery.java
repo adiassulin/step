1342320ec94aedfcba2a20c15784626f71386dfd
adiassulin <adiassulin@gmail.com>
2020-08-24 15:01:59 +0000

    findMeetingQuery implementation

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,77 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.*;
+import java.util.Comparator; 
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+      //more than one day duration 
+      if (request.getDuration() > TimeRange.WHOLE_DAY.duration())
+      {
+          return Arrays.asList();
+      }
+
+      //no meetings on this day
+      if (events.isEmpty())
+      {
+          return Arrays.asList(TimeRange.WHOLE_DAY);
+      }
+
+      //no attendees in request
+      if (request.getAttendees().isEmpty())
+      {
+          return Arrays.asList(TimeRange.WHOLE_DAY);
+      }
+
+      //todo: what about negative duration ? 
+
+      Set<TimeRange> notAvialableTimes = new HashSet<>();
+      for (Event e : events)
+      {
+          if (attendeesInEvent(e.getAttendees(), request.getAttendees()))
+          {
+              notAvialableTimes.add(e.getWhen());
+          }
+      }
+
+      return createAvailavleTimes(notAvialableTimes, request);
+    // throw new UnsupportedOperationException("TODO: Implement this method.");
+  }
+
+//TODO - implement 
+
+  private Collection<TimeRange> createAvailavleTimes(Set<TimeRange> notAvialableTimes, MeetingRequest request) {
+      int currentStart = TimeRange.START_OF_DAY;
+      int currentEnd;
+      Collection<TimeRange> avialableTimes = new HashSet<>();
+      Collection.sort(notAvialableTimes, TimeRange.ORDER_BY_START);
+
+      for (TimeRange eventTime: notAvialableTimes)
+      {
+          currentEnd = eventTime.start();
+          if (request.getDuration() <= currentEnd - currentStart)
+          {
+              avialableTimes.add(TimeRange.fromStartDuration(currentStart, currentEnd - currentStart));
+          }
+
+          currentStart = eventTime.end();
+      }
+
+      if (TimeRange.END_OF_DAY - currentStart >= request.getDuration())
+      {
+          avialableTimes.add(TimeRange.fromStartDuration(currentStart, TimeRange.END_OF_DAY - currentStart));
+      }
+      return avialableTimes;
+  }
+  
+  private boolean attendeesInEvent(Set<String> eventAtendees, Collection<String> requestAtendees)
+  {
+      for (String name: requestAtendees)
+      {
+          if (eventAtendees.contains(name)) return true;
+      }
+      
+      return false;
   }
 }
